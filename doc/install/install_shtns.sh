d78cccc1ea715dc6f41703334465fdc17ee71ddb
Ashwin Vishnu <avmo@kth.se>
2019-01-03 15:48:14 +0100

    Try clang to install shtns for pipelines

--- doc/install/install_shtns.sh
+++ doc/install/install_shtns.sh
@@ -2,6 +2,6 @@
 hg clone https://bitbucket.org/nschaeff/shtns
 cd shtns
 hg update v3.1
-./configure --enable-openmp --enable-python
-make
+CC=clang ./configure --enable-openmp --enable-python
+make -j
 python setup.py install
