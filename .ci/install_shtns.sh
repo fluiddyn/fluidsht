378da37de3b81db33f08ed93abbd50038cd3e3c6
Ashwin Vishnu <avmo@kth.se>
2019-01-03 15:55:48 +0100

    Disable simd in pipelines

--- .ci/install_shtns.sh
+++ .ci/install_shtns.sh
@@ -2,6 +2,6 @@
 hg clone https://bitbucket.org/nschaeff/shtns
 cd shtns
 hg update v3.1
-./configure --enable-openmp --enable-python
+./configure --enable-openmp --enable-python --disable-simd
 make -j
 python setup.py install
