fb1dfe36becfc00504a7f24e3c774003ccb9a671
Ashwin Vishnu <avmo@kth.se>
2019-01-23 17:21:32 +0100

    Add user install shtns script

---
+++ .ci/install_shtns_user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+hg clone https://bitbucket.org/nschaeff/shtns
+cd shtns
+hg update v3.1
+./configure --enable-openmp --enable-python --disable-simd
+make -j
+python setup.py install --user
